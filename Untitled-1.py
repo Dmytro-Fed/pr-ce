5280f4627b9f7f42f60b7eb2b07ccc985034ee42
Dmytro-Fed <dmytro.fedoryshchak@infis.cz>
2025-03-11 08:35:14 +0100

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,19 @@
+barva = input("Jake je vasa oblibena barva")    #Načtení od uživatele
+if barva == "červena":                          #if
+    print("Červena je dobrá")                   #Vypsat 1(a)
+elif barva == "Modrá":                          #elif
+    print("Modrá je špatná")                    #Vypsat 1(b)
+else:                                           #else
+    print("nice")                               #Vypsat 1(c)
+číslo1 = int(input("Zadejte číslo 1"))          #Přetypování 1
+číslo2 = int(input("Zadejte číslo 2"))          #Přetypování 2
+součet = číslo1 + číslo2                        #Logické operátory    
+print(součet)                                   #Vypsat 2
+
+#git příklad 1:git add *
+#git příklad 2:git push
+#git příklad 3:git pull
+#git příklad 4:git clone
+#git příklad 5:git commit - m "text"
+
+#Dmytro Fedoryshchak 1.B (1,a)
